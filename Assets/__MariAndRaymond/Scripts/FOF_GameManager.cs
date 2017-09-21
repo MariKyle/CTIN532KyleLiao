35034341eb4d2016e006365aaae409055ac38cbe
Raymond Sheng Liao <raymondliaonlft@gmail.com>
2017-09-20 20:01:48 -0700

    Commit on 9.20

--- Assets/__MariAndRaymond/Scripts/FOF_GameManager.cs
+++ Assets/__MariAndRaymond/Scripts/FOF_GameManager.cs
@@ -8,12 +8,15 @@ public class FOF_GameManager : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-		
+        Screen.SetResolution(1920, 1080, true);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
 	}
 }
