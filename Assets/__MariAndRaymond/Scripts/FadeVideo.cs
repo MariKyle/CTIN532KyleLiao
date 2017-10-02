d6fb367b0546f054c54bacb00d44021fbb89e76e
Mari Kyle <Marimkyle@gmail.com>
2017-10-02 00:02:45 -0700

    Added title sequence including: ambience audio, narrative voice over, Table V2, title, flickering lights.

---
+++ Assets/__MariAndRaymond/Scripts/FadeVideo.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class FadeVideo : MonoBehaviour {
+
+
+    public bool fadeOut = false;
+    public bool fadeIn = true;
+    public float t = 0.0f;
+
+    public void Start()
+    {
+        //fadeOut = false;
+        //fadeIn = false;
+    }
+    public void Update()
+    {
+        if (fadeOut)
+        {
+            fadeVideo();
+        }
+
+        if (fadeIn)
+        {
+            fadeVideo(false);
+        }
+    }
+
+    void fadeVideo(bool fadeOutVideo = true)
+    {
+        t += 0.5f * Time.deltaTime;
+
+        Color color = GetComponent<Renderer>().material.color;
+
+        if (fadeOutVideo)
+            color.a = Mathf.Lerp(1, 0, t);
+        else
+            color.a = Mathf.Lerp(0, 1, t);
+
+
+        //Debug.Log("color.a: " + color.a.ToString());
+
+        GetComponent<Renderer>().material.SetColor("_Color", color);
+
+        if (t >= 1)
+        {
+            t = 0.0f;
+
+            if (fadeOutVideo)
+                fadeOut = false;
+            else
+                fadeIn = false;
+        }
+    }
+
+}
---
+++ Assets/__MariAndRaymond/Scripts/Timer.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+	//font
+	public GameObject title;
+	public GameObject title1;
+	public GameObject title2;
+
+	//audio
+	public GameObject WineTink;
+	public GameObject Laugh;
+	public GameObject Jazz;
+	public GameObject Restaurant;
+	public GameObject Open;
+	public GameObject Open2;
+	public GameObject Thunder;
+
+	//lights
+	public GameObject light1;	
+	public GameObject light2;
+	public GameObject light3;	
+	public GameObject light4;
+	public GameObject light5;
+
+
+	//objects
+	public GameObject Wine;
+	public GameObject Glasses;
+	public GameObject Silverware;
+	public GameObject ServingDishes;
+	public GameObject Table;
+	public GameObject chairs;
+
+	public GameObject turkey;
+	public GameObject turkey2;
+	public GameObject Bread;
+
+	public GameObject Grape1;
+	public GameObject Grape2;
+	public GameObject Grape3;
+	public GameObject Grape4;
+	public GameObject Grape5;
+
+	public GameObject Plate1;
+	public GameObject Placemat1;
+	public GameObject Plate2;
+	public GameObject Placemat2;
+	public GameObject Plate3;
+	public GameObject Placemat3;
+	public GameObject Plate4;
+	public GameObject Placemat4;
+	public GameObject Plate5;
+	public GameObject Placemat5;
+	public GameObject Plate6;
+	public GameObject Placemat6;
+
+
+		private void Start()
+		{
+			StartCoroutine(ActivationRoutine());
+		}
+
+		private IEnumerator ActivationRoutine()
+		{        
+		yield return new WaitForSeconds(2);
+		WineTink.SetActive(true);
+		Wine.SetActive (true);
+		title.SetActive (true);
+
+		yield return new WaitForSeconds(1);
+		Laugh.SetActive(true);
+
+		yield return new WaitForSeconds(1);
+		Glasses.SetActive(true);
+
+		yield return new WaitForSeconds(2);
+		Restaurant.SetActive (true);
+		turkey.SetActive(true);
+		Grape1.SetActive (true);
+
+		yield return new WaitForSeconds(1);
+		Jazz.SetActive(true);
+		Plate1.SetActive (true);
+		Placemat1.SetActive (true);
+		Grape2.SetActive (true);
+
+
+		yield return new WaitForSeconds(1);
+		Plate2.SetActive (true);
+		Placemat2.SetActive (true);
+		Grape3.SetActive (true);
+		turkey2.SetActive (true);
+		Open.SetActive (true);
+
+
+		yield return new WaitForSeconds(1);
+		Plate3.SetActive (true);
+		Placemat3.SetActive (true);
+		Grape4.SetActive (true);
+		Silverware.SetActive (true);
+		title.SetActive (false);
+
+
+		yield return new WaitForSeconds(1);
+		Plate4.SetActive (true);
+		Placemat4.SetActive (true);
+		Grape5.SetActive (true);
+
+		yield return new WaitForSeconds(1);
+		Plate5.SetActive (true);
+		Placemat5.SetActive (true);
+		ServingDishes.SetActive (true);
+
+		yield return new WaitForSeconds(1);
+		Plate6.SetActive (true);
+		Placemat6.SetActive (true);
+		Bread.SetActive (true);
+
+		yield return new WaitForSeconds(1);
+		Table.SetActive (true);
+		chairs.SetActive (true);
+		title1.SetActive (true);
+
+		yield return new WaitForSeconds(10);
+		title1.SetActive (false);
+
+		yield return new WaitForSeconds(8);
+		Thunder.SetActive (true);
+		title2.SetActive (true);
+		light1.SetActive (true);
+		light2.SetActive (true);
+		light3.SetActive (true);
+		light4.SetActive (true);
+		light5.SetActive (true);
+
+		yield return new WaitForSeconds(2);
+		Open2.SetActive (true);
+
+		yield return new WaitForSeconds(14);
+		Application.LoadLevel ("FeastOfFools_Scene1_Table");
+
+
+		}
+	}
+
