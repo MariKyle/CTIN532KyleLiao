7e2be83721349176073647ad17c3a272dc014923
Mari Kyle <Marimkyle@gmail.com>
2017-10-10 00:11:15 -0700

    Skeleton for Start Menu. Start, Tutorial, Quit, Credit buttons added. Envelope Image Added. New Scene "Menu" Added.

---
+++ Assets/__MariAndRaymond/Scripts/LoadLevelM.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevelM : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update(){
+		if(Input.GetMouseButton(0))
+		{
+			Application.LoadLevel("Title Sequence");
+		}
+	}
+}
