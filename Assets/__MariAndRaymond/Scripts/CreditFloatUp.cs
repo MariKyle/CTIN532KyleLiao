a5895b0ea8a77ec7fceb28d1e7b6d5a41f2bfbaf
Mari Kyle <Marimkyle@gmail.com>
2017-10-30 00:06:19 -0700

    Added Ending credits including: new font, ending audio, and credit script.

---
+++ Assets/__MariAndRaymond/Scripts/CreditFloatUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditFloatUp : MonoBehaviour {
+
+	// Use this for initialization
+
+
+
+		void Start () {
+
+		}
+
+	void Update() {
+		transform.Translate(Vector3.up * 17 * Time.deltaTime, Space.World);
+	}
+
+}
